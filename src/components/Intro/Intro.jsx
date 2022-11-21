251e2796877198ec89f1f37c5382a15433f4ca62
Artyom Rusakov <popsninka@gmail.com>
2022-11-21 17:27:01 +0300

    Feature/PW-2-Intro (#2)
    * Rename index.jsx to main.jsx
    
    * Add Intro component
    
    * Import Intro component to App
    
    * Add styling to Intro component

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,17 @@
+const Intro = () => {
+    return (
+        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+            <h1 className="text-4xl md:text-7xl pt-20 pb-6 font-bold">Rusakov Artyom</h1>
+            <p className="text-base md:text-xl mb-3 font-medium">A novice frontend developer</p>
+            <p className="text-sm max-w-xl mb-6 font-bold">I'm a novice react developer from Russia who's trying to get his first job in IT.
+                <br />
+                Constantly learning new technologies and tools for web-development
+                <br />
+                You can contact via Telegram, LinkedIn, Headhunter and mail.
+                <br />
+                Also here's my GitHub profile,
+            </p>
+        </div>
+    );
+};
+export default Intro;
