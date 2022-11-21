5fbcd6b413c2048c918454a67b43cacd4378e7b0
Artyom Rusakov <popsninka@gmail.com>
2022-11-22 01:03:40 +0300

    Add Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+        <div className="py-5 text-center">
+            <p className="text-sm mt-2 opacity-50">&copy; { new Date().getFullYear() } Rusakov Artyom. All rights reserved.</p>
+        </div>
+    );
+};
+export default Footer;
