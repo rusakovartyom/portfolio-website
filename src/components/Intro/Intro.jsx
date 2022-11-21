1fad9c084307a838d4292231b5e9520cf0ad3cc6
Artyom Rusakov <popsninka@gmail.com>
2022-11-21 17:13:21 +0300

    Add Intro component

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,17 @@
+const Intro = () => {
+    return (
+        <div >
+            <h1 >Rusakov Artyom</h1>
+            <p >A novice frontend developer</p>
+            <p >I'm a novice react developer from Russia who's trying to get his first job in IT.
+                <br />
+                Constantly learning new technologies and tools for web-development
+                <br />
+                You can contact via Telegram, LinkedIn, Headhunter and mail.
+                <br />
+                Also here's my GitHub profile
+            </p>
+        </div>
+    );
+};
+export default Intro;
