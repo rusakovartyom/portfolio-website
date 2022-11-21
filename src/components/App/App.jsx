692d64ebc927e8341756eaa601ce266087f8b147
Artyom Rusakov <popsninka@gmail.com>
2022-11-22 01:05:57 +0300

    Import Footer component to App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import Intro from "../Intro";
 import Portfolio from "../Portfolio";
+import Footer from "../Footer";
 
 const App = () => {
   return (
     <div>
       <Intro />
       <Portfolio />
+      <Footer />
     </div>
   );
 };
