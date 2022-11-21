d695590508b287be9405b7fec37e096fe145206c
Artyom Rusakov <popsninka@gmail.com>
2022-11-21 17:30:29 +0300

    Add data for Portfolio component to display

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: 'Simple Trello',
+    imgURL: '',
+    stack: ['React', 'Redux'],
+    link: 'https://rusakov-trello.netlify.app/',
+  },
+  {
+    title: 'Personal Portfolio',
+    imgURL: '',
+    stack: ['React', 'Tailwind', 'Vite'],
+    link: 'https://rusakov-portfolio.netlify.app/',
+  },
+  {
+    title: 'Blog',
+    imgURL: '',
+    stack: ['React', 'Next.js', 'Firebase'],
+    link: 'https://rusakov-next-blog.netlify.app/',
+  },
+];
