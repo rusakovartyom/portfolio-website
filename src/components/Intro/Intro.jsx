1a9eb7294ba369fc30fa5eceff318f0b6718e15d
Artyom Rusakov <popsninka@gmail.com>
2022-11-22 01:23:51 +0300

    Edit styles and text for Intro

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,15 +1,9 @@
 const Intro = () => {
     return (
-        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-            <h1 className="text-4xl md:text-7xl pt-20 pb-6 font-bold">Rusakov Artyom</h1>
-            <p className="text-base md:text-xl mb-3 font-medium">A novice frontend developer</p>
-            <p className="text-sm max-w-xl mb-6 font-bold">I'm a novice react developer from Russia who's trying to get his first job in IT.
-                <br />
-                Constantly learning new technologies and tools for web-development
-                <br />
-                You can contact via Telegram, LinkedIn, Headhunter and mail.
-                <br />
-                Also here's my GitHub profile,
+        <div className="flex items-center justify-center flex-col text-center pt-20 pb-5">
+            <h1 className="text-4xl md:text-5xl pt-5 pb-2 font-bold">Rusakov Artyom</h1>
+            <p className="text-xl md:text-2xl mb-9 font-medium">React Frontend Developer</p>
+            <p className="text-base px-5 md:px-0 max-w-xl mb-6 font-medium">I'm a novice web developer from Russia who's trying to get his first job in IT. Constantly learning new technologies and tools for web-development.
             </p>
         </div>
     );
