5f235e9c215c22384fae09627022f445f6c519d3
Artyom Rusakov <popsninka@gmail.com>
2022-11-22 02:59:29 +0300

    Feature/PW-7-dark-mode-support (#8)
    * Add functionality for light and dark modes, edit styles for App component
    
    * Update styles for dark mode support, edit bio
    
    * Edit styles for dark mode support

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,11 +1,21 @@
 const Intro = () => {
-    return (
-        <div className="flex items-center justify-center flex-col text-center pt-20 pb-5">
-            <h1 className="text-4xl md:text-5xl pt-5 pb-2 font-bold">Rusakov Artyom</h1>
-            <p className="text-xl md:text-2xl mb-9 font-medium">React Frontend Developer</p>
-            <p className="text-base px-5 md:px-0 max-w-xl mb-6 font-medium">I'm a novice web developer from Russia who's trying to get his first job in IT. Constantly learning new technologies and tools for web-development.
-            </p>
-        </div>
-    );
+  return (
+    <div className=" flex items-center justify-center flex-col text-center pt-20 pb-5">
+      <h1 className="transition delay-150 text-4xl md:text-5xl text-neutral-900 dark:text-neutral-100 pt-5 pb-2 font-bold">
+        Rusakov Artyom
+      </h1>
+      <p className="transition delay-150 text-xl md:text-2xl mb-9 font-medium">
+        React Frontend Developer
+      </p>
+      <p className="transition delay-150 text-base px-5 md:px-0 max-w-xl mb-6 font-medium">
+        I'm a novice web developer from Russia, Saint-Petersburg. Constantly
+        learning new technologies and tools for web-development.
+      </p>
+      <p className="transition delay-150 text-base px-5 md:px-0 max-w-xl mb-6 font-medium">
+        Welcome to my portfolio website. I built it with React, Tailwind CSS and
+        Vite.
+      </p>
+    </div>
+  );
 };
 export default Intro;
--- src/components/Portfolio/PortfolioItem/PortfolioItem.jsx
+++ src/components/Portfolio/PortfolioItem/PortfolioItem.jsx
@@ -1,16 +1,27 @@
 const PortfolioItem = ({ title, imgURL, imgAlt, stack, link }) => {
-    return (
-        <div className="border-2 border-stone-900 rounded-md overflow-hidden">
-            <img className="w-full h-36 md:h-48 object-cover cursor-pointer" src={ imgURL } alt={ imgAlt } />
-            <div className="w-full p-4">
-                <h3 className="text-lg md:text-xl mb-2 md:text-xl mb-2 md:mb-3 font-bold">{ title }</h3>
-                <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">{ stack.map(item => (
-                    <span key={ item } className="inline-block px-2 py-1 font-bold border-2 border-stone-900 rounded-md">
-                        { item }
-                    </span>
-                )) }</p>
-            </div>
-        </div >
-    );
+  return (
+    <div className="transition delay-150 border-2 border-neutral-900 dark:border-neutral-100 rounded-md overflow-hidden">
+      <img
+        className="w-full h-36 md:h-48 object-cover cursor-pointer"
+        src={imgURL}
+        alt={imgAlt}
+      />
+      <div className="w-full p-4">
+        <h3 className="text-lg md:text-xl dark:text-neutral-100 mb-2 md:text-xl mb-2 md:mb-3 font-bold">
+          {title}
+        </h3>
+        <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-neutral-100">
+          {stack.map((item) => (
+            <span
+              key={item}
+              className="transition delay-150 inline-block px-2 py-1 font-bold border-2 border-neutral-900 dark:border-neutral-100 rounded-md"
+            >
+              {item}
+            </span>
+          ))}
+        </p>
+      </div>
+    </div>
+  );
 };
 export default PortfolioItem;
